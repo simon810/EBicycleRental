4b6c11aad0ce7d5252ed8b1de1a778d267b2bb1f
simon810 <sweldemichael6@gmail.com>
2021-05-27 12:08:23 -0700

    sql.data upate

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -23,22 +23,22 @@ INSERT INTO  vehicles (vehicle_id, make, model, plate_number, vehicle_number, ye
 INSERT INTO  vehicles (vehicle_id, make, model, plate_number, vehicle_number, year,category_id) VALUES (9, 'Super', 'Super', '89735', 'VHL9', 2014,3);
 INSERT INTO  vehicles (vehicle_id, make, model, plate_number, vehicle_number, year,category_id) VALUES (10, 'Jegante', 'Jegante', '89735', 'VHL10', 2018,6);
 
-INSERT INTO  credentials (credential_id, password, user_name) VALUES (1, '$2y$10$.MU0VSB8Z.Qmgg2bz6pZLO4cwjAF/iKTH7QjWEaayV0YztsRYgcha', 'admin');
-INSERT INTO  credentials (credential_id, password, user_name) VALUES (2, '$2y$10$.MU0VSB8Z.Qmgg2bz6pZLO4cwjAF/iKTH7QjWEaayV0YztsRYgcha', 'staff');
--- INSERT INTO  credentials (credential_id, password, user_name) VALUES (3, '$2y$10$.MU0VSB8Z.Qmgg2bz6pZLO4cwjAF/iKTH7QjWEaayV0YztsRYgcha', 'customer');
+INSERT INTO  credentials (credential_id, password, user_name) VALUES (10, '$2y$10$.MU0VSB8Z.Qmgg2bz6pZLO4cwjAF/iKTH7QjWEaayV0YztsRYgcha', 'admin');
+INSERT INTO  credentials (credential_id, password, user_name) VALUES (20, '$2y$10$.MU0VSB8Z.Qmgg2bz6pZLO4cwjAF/iKTH7QjWEaayV0YztsRYgcha', 'staff');
+INSERT INTO  credentials (credential_id, password, user_name) VALUES (30, '$2y$10$.MU0VSB8Z.Qmgg2bz6pZLO4cwjAF/iKTH7QjWEaayV0YztsRYgcha', 'customer');
 
 
-INSERT INTO  users (user_id, date_of_birth, first_name, last_name, license_number, middle_name, credential_id) VALUES (1, '1994-09-09', 'Essey', 'Tezare', '98765432', 'Abraham',1);
-INSERT INTO  users (user_id, date_of_birth, first_name, last_name, license_number, middle_name, credential_id) VALUES (2, '1993-09-09', 'Dawit', 'Hailu', '98756765', 'Tesfahannes',2);
--- INSERT INTO  users (user_id, date_of_birth, first_name, last_name, license_number, middle_name, credential_id) VALUES (3, '1993-01-09', 'Biniam', 'Arefaine', '98765376', 'Tsehaye',3);
+INSERT INTO  users (user_id, date_of_birth, first_name, last_name, license_number, middle_name, credential_id) VALUES (10, '1994-09-09', 'Essey', 'Tezare', '98765432', 'Abraham',10);
+INSERT INTO  users (user_id, date_of_birth, first_name, last_name, license_number, middle_name, credential_id) VALUES (20, '1993-09-09', 'Dawit', 'Hailu', '98756765', 'Tesfahannes',20);
+INSERT INTO  users (user_id, date_of_birth, first_name, last_name, license_number, middle_name, credential_id) VALUES (30, '1993-01-09', 'Biniam', 'Arefaine', '98765376', 'Tsehaye',30);
 
 -- INSERT INTO  bookings (booking_id, booking_date, date_of_birth, email, end_date, first_name, last_name, license_number, reference_number, start_date, total_price, payment_id, user_id, vehicle_id) VALUES ('1', '2009-09-09', '1993-09-11', 'binireyes@gmail.com', '2009-09-12', 'Biniam', 'Arefaine', '123456789', '100', '2009-09-09', '320.00', '1', '1', '1');
 
 INSERT INTO role(role_id, role_name) VALUES (1, 'ROLE_ADMIN');
 INSERT INTO role(role_id, role_name) VALUES (2, 'ROLE_STAFF');
--- INSERT INTO role(role_id, role_name) VALUES (3, 'ROLE_CUSTOMER');
+INSERT INTO role(role_id, role_name) VALUES (3, 'ROLE_CUSTOMER');
 
-INSERT INTO user_roles (user_id, role_id) VALUES (1, 1);
-INSERT INTO user_roles (user_id, role_id) VALUES (2, 2);
--- INSERT INTO user_roles (user_id, role_id) VALUES (3, 3);
+INSERT INTO user_roles (user_id, role_id) VALUES (10, 1);
+INSERT INTO user_roles (user_id, role_id) VALUES (20, 2);
+INSERT INTO user_roles (user_id, role_id) VALUES (30, 3);
 
